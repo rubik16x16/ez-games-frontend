33561a929d990a8a4ca1a1e835377f6a7be4ab3e
Anthony <drt_mike@hotmail.com>
2022-06-16 18:46:50 -0400

    Remake Mytournaments card

--- src/app/modules/user/pages/my-tournaments/my-tournaments.component.ts
+++ src/app/modules/user/pages/my-tournaments/my-tournaments.component.ts
@@ -3,34 +3,25 @@ import { Tournament } from 'src/app/models/tournament';
 import { MyTournamentsService } from 'src/app/services/my-tournaments.service';
 
 @Component({
-  selector: 'app-my-tournaments',
-  templateUrl: './my-tournaments.component.html',
-  styleUrls: ['./my-tournaments.component.scss']
+	selector: 'app-my-tournaments',
+	templateUrl: './my-tournaments.component.html',
+	styleUrls: ['./my-tournaments.component.scss']
 })
 export class MyTournamentsComponent implements OnInit {
 
-	myTournaments: any[] = [
-		{
-			id: 1,
-			name: 'test'
-		},
-		{
-			id: 1,
-			name: 'test'
-		}
-	];
+	myTournaments: any[];
 
-  constructor(
-  	private myTournamentsService: MyTournamentsService
-  ) { }
+	constructor(
+		private myTournamentsService: MyTournamentsService
+	) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 
-  	this.myTournamentsService.list().subscribe(res => {
+		this.myTournamentsService.list().subscribe(res => {
 
-  		console.log(res);
-  		this.myTournaments = res;
-  	});
-  }
+			console.log(res);
+			this.myTournaments = res;
+		});
+	}
 
 }
